490cd7f059ba68b5c721ec47b0e393ac1b95f943
dinesh <dinesh@tradelab.in>
2023-08-18 14:17:12 +0530

    add footer

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import "../styles.css"
 import Login from './Login';
 import Navbar from './Navbar';
+import Footer from './Footer';
 
 function App() {
   return (
   <div>
     <Navbar/>
+    <Footer/>
     {/* <Login /> */}
     </div>
   )
